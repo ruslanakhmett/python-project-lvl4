7a72cd28bcfb7197abe412ae0773026a3da3bee8
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-08-04 18:00:20 +0300

    step 3.0

--- task_manager/web/urls.py
+++ task_manager/web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, LoginPageView, SignUpView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('users/create', SignUpView.as_view(), name='signup'),
 
 ]
--- task_manager/web/views.py
+++ task_manager/web/views.py
@@ -1,9 +1,42 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
-from django.utils.translation import gettext as _
+from django.contrib.auth import login, logout
+from .forms import UserLoginForm, SignUpForm
+from django.contrib.auth import authenticate
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'pages/index.html')
+
+
+class LoginPageView(View):
+    template_name = 'pages/login.html'
+    form = UserLoginForm()
+
+    def get(self, request):
+        return render(request, self.template_name, context={'form': self.form})
+
+    def post(self, request):
+        form = self.form(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = 'Вы залогинены'
+            else:
+                message = 'Пожалуйста, введите правильные имя пользователя и пароль. Оба поля могут быть чувствительны к регистру.'
+        return render(
+        request, self.template_name, context={'form': form, 'message': message})
+        
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'pages/signup.html'
