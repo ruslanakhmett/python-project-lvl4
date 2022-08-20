8feaff5ff2fd06d3df93a0a78d9e7c0b16f0f1ea
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-08-20 16:08:40 +0300

    test tests

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'webserver']
 
 
 INSTALLED_APPS = [
