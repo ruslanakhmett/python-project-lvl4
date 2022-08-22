3fcf60233def54d0ac918dc5d0750ccdeb169a75
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-08-22 16:05:33 +0300

    test 47

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -73,6 +73,14 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 #     }
 # }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 AUTH_PASSWORD_VALIDATORS = [
